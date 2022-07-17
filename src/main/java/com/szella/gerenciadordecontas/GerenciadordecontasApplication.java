9f865bb961aca5d17d3918803b605d0eb3494982
Zella <sergioluiz.zella@gmail.com>
2022-07-16 23:08:08 -0300

    Primeiro commit

---
+++ src/main/java/com/szella/gerenciadordecontas/GerenciadordecontasApplication.java
@@ -0,0 +1,13 @@
+package com.szella.gerenciadordecontas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordecontasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordecontasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szella/gerenciadordecontas/GerenciadordecontasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szella.gerenciadordecontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordecontasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
